f5ce66f400b55e9f7f8536c03398297c70d2a0d9
me11ong <huijih8@gmail.com>
2023-01-26 14:49:04 +0900

    52. useState 도입, 버튼 누르면 제목이 바뀌도록

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,27 @@
+import React, {useState} from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "./Card";
 
-function ExpenseItem(props) {
+function ExpenseItem(props) 
+{
+  
+  const [title,setTitle]=useState(props.title);
 
-  const clickHandler=()=>
-  {console.log('Button Clicked!');}
+  const clickHandler = () =>
+  {
+      setTitle('Clicked!');
+  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="expense-item__price">${props.amount}</div>
-      <button onClick={clickHandler()}>Click Here</button>
+      <button onClick={clickHandler}>Click To Update!</button>
     </Card>
   );
 }
